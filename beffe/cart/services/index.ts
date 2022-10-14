026f7d1acd99653a89bf415a4168de11671b47f0
prathyushapallapothula <prathyusha.pallapothula@zemosolabs.com>
2022-10-14 12:35:57 +0530

    add product and cart apis

--- beffe/cart/services/index.ts
+++ beffe/cart/services/index.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { properties } from "../../properties";
 
-export const getCalenderEvent = (userID: string): Promise<any> => {
+
+export const getCartItems = (userID: string): Promise<any> => {
+
     return new Promise<any>((resolve, reject) => {
         axios({
             method: 'get',
-            url: `${properties.DB_URL}/events?userID=${userID}`
+            url: `${properties.DB_URL}/cart?userID=${userID}`
         })
         .then(res => {
             resolve(res.data)
@@ -18,18 +20,17 @@ export const getCalenderEvent = (userID: string): Promise<any> => {
 
 }
 
-export const createCalenderEvent = (request: any): Promise<any> => {
+export const addItemtoCart = (request: any): Promise<any> => {
     let data: any = {
-        userID: request.userID
+        userID: request.userID,
+        productID: request. productID,
+        quantity: request.quantity
     }
-    if(request.title) data['title'] = request.title
-    if(request.start) data['start'] = request.start
-    if(request.end) data['end'] = request.end
 
     return new Promise<any>((resolve, reject) => {
         axios({
             method: 'post',
-            url: `${properties.DB_URL}/events`,
+            url: `${properties.DB_URL}/cart`,
             data
         })
         .then(res => {
@@ -42,43 +43,40 @@ export const createCalenderEvent = (request: any): Promise<any> => {
     })
 }
 
-export const updateCalenderEvent = (request: any): Promise<any> => {
-    let data: any = {}
-    if(request.title) data['title'] = request.title
-    if(request.start) data['start'] = request.start
-    if(request.end) data['end'] = request.end
-
-    return new Promise<any>((resolve, reject) => {
-        axios({
-            method: 'patch',
-            url: `${properties.DB_URL}/events/${request.eventID}`,
-            data
-        })
-        .then(res => {
-            resolve(res.data.id)
-        })
-        .catch((err) => {
-            console.log(err)
-            reject(err)
-        });
-    })
-}
-
-
-export const deleteCalenderEvent = (eventID: string): Promise<any> => {
+export const removeItemFromCart = (cartItemID: string): Promise<any> => {
 
     return new Promise<any>((resolve, reject) => {
         axios({
             method: 'delete',
-            url: `${properties.DB_URL}/events/${eventID}`
+            url: `${properties.DB_URL}/cart/${cartItemID}`,
         })
         .then(res => {
-            resolve(eventID)
+            resolve(cartItemID)
         })
         .catch((err) => {
-            // console.log("in err...", err.response)
+            console.log(err)
             reject(err)
         });
     })
 }
 
+export const updateCartItem = (request: any): Promise<any> => {
+    let data = {
+        quantity: request.quantity
+    }
+
+    return new Promise<any>((resolve, reject) => {
+        axios({
+            method: 'patch',
+            url: `${properties.DB_URL}/cart/${request.cartItemID}`,
+            data
+        })
+        .then(res => {
+            resolve(res.data.id)
+        })
+        .catch((err) => {
+            console.log(err)
+            reject(err)
+        });
+    })
+}
