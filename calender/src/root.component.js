3c45b8a638b527a6c9a45417a7c615576e3c8d5b
yashwanthyadav1 <yashwanth.parupudi@zemosolabs.com>
2022-10-14 13:05:19 +0530

    Merge branch 'master' of https://github.com/sampathbabu1/Single-SPA

--- calender/src/root.component.js
+++ calender/src/root.component.js
@@ -1,5 +1,12 @@
 import Calender from "./Calender";
+import ReactDOM from "react-dom";
+import { createClient, Provider } from 'urql';
+
+const client = createClient({
+  url: 'http://localhost:4000/',
+});
 
 export default function Root(props) {
-  return <Calender />;
+  return <Provider value={client}><Calender />
+  </Provider>;
 }
