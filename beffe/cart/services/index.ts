296df9d2592086ed2cf497c99c9c3ac1b600ab68
iamkiran27 <saikiran.kavali@zemosolabs.com>
2022-10-17 11:34:02 +0530

    Cart-graphql done

--- beffe/cart/services/index.ts
+++ beffe/cart/services/index.ts
@@ -1,82 +1,84 @@
 import axios from "axios";
 import { properties } from "../../properties";
 
-
 export const getCartItems = (userID: string): Promise<any> => {
+  console.log("Request made!!!!");
 
-    return new Promise<any>((resolve, reject) => {
-        axios({
-            method: 'get',
-            url: `${properties.DB_URL}/cart?userID=${userID}`
-        })
-        .then(res => {
-            resolve(res.data)
-        })
-        .catch((err) => {
-            console.log(err)
-            reject(err)
-        });
+  return new Promise<any>((resolve, reject) => {
+    axios({
+      method: "get",
+      url: `${properties.DB_URL}/cart?userID=${userID}`,
     })
-
-}
+      .then((res) => {
+        resolve(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        reject(err);
+      });
+  });
+};
 
 export const addItemtoCart = (request: any): Promise<any> => {
-    let data: any = {
-        userID: request.userID,
-        productID: request. productID,
-        quantity: request.quantity
-    }
+  const data: any = {
+    userID: request.userID,
+    productID: request.productID,
+    quantity: request.quantity,
+  };
 
-    return new Promise<any>((resolve, reject) => {
-        axios({
-            method: 'post',
-            url: `${properties.DB_URL}/cart`,
-            data
-        })
-        .then(res => {
-            resolve(res.data.id)
-        })
-        .catch((err) => {
-            console.log(err)
-            reject(err)
-        });
+  return new Promise<any>((resolve, reject) => {
+    axios({
+      method: "post",
+      url: `${properties.DB_URL}/cart`,
+      data,
     })
-}
+      .then((res) => {
+        resolve(res.data.id);
+      })
+      .catch((err) => {
+        console.log(err);
+        reject(err);
+      });
+  });
+};
 
 export const removeItemFromCart = (cartItemID: string): Promise<any> => {
+  console.log("Inside delete cart");
 
-    return new Promise<any>((resolve, reject) => {
-        axios({
-            method: 'delete',
-            url: `${properties.DB_URL}/cart/${cartItemID}`,
-        })
-        .then(res => {
-            resolve(cartItemID)
-        })
-        .catch((err) => {
-            console.log(err)
-            reject(err)
-        });
+  return new Promise<any>((resolve, reject) => {
+    axios({
+      method: "delete",
+      url: `${properties.DB_URL}/cart/${cartItemID}`,
     })
-}
+      .then((res) => {
+        resolve(cartItemID);
+      })
+      .catch((err) => {
+        console.log(err);
+        reject(err);
+      });
+  });
+};
 
 export const updateCartItem = (request: any): Promise<any> => {
-    let data = {
-        quantity: request.quantity
-    }
+  console.log("Inside update cart item");
 
-    return new Promise<any>((resolve, reject) => {
-        axios({
-            method: 'patch',
-            url: `${properties.DB_URL}/cart/${request.cartItemID}`,
-            data
-        })
-        .then(res => {
-            resolve(res.data.id)
-        })
-        .catch((err) => {
-            console.log(err)
-            reject(err)
-        });
+  const data = {
+    quantity: request.quantity,
+  };
+
+  return new Promise<any>((resolve, reject) => {
+    axios({
+      method: "patch",
+      url: `${properties.DB_URL}/cart/${request.cartItemID}`,
+      data,
     })
-}
+      .then((res) => {
+        resolve(res.data.id);
+      })
+      .catch((err) => {
+        console.log(err);
+        reject(err);
+      });
+  });
+};
--- beffe/product/services/index.ts
+++ beffe/product/services/index.ts
@@ -2,28 +2,28 @@ import axios from "axios";
 import { properties } from "../../properties";
 
 export const getProducts = (getProductsInput: any): Promise<any> => {
+  const ids = getProductsInput?.ids;
 
-    const ids = getProductsInput?.ids
+  console.log("Ids are : ", ids);
 
-    let url = `${properties.DB_URL}/products`
-    let querystring = "";
-    if(ids && ids.length > 0) {
-        querystring = "id=" + ids.join("&id=")
-        url = url + `?${querystring}`
-    }
+  let url = `${properties.DB_URL}/products`;
+  let querystring = "";
+  if (ids && ids.length > 0) {
+    querystring = "id=" + ids.join("&id=");
+    url = url + `?${querystring}`;
+  }
 
-    return new Promise<any>((resolve, reject) => {
-        axios({
-            method: 'get',
-            url
-        })
-        .then(res => {
-            resolve(res.data)
-        })
-        .catch((err) => {
-            console.log(err)
-            reject(err)
-        });
+  return new Promise<any>((resolve, reject) => {
+    axios({
+      method: "get",
+      url,
     })
-
-}
+      .then((res) => {
+        resolve(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        reject(err);
+      });
+  });
+};
