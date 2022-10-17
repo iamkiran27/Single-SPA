5bf69524735e1ef480116ef945f8fe5a31ce9af3
iamkiran27 <saikiran.kavali@zemosolabs.com>
2022-10-17 07:14:24 +0530

    gql=cart

--- cart/src/test.jsx
+++ cart/src/test.jsx
@@ -12,54 +12,56 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import { navigateToUrl } from "single-spa";
 
 const cart = () => {
-  const [products, setProducts] = useState([]);
-  const [cartItems, setCartItems] = useState(
-    JSON.parse(localStorage.getItem("cart"))
-  );
-  const [sum, setSum] = useState(0);
-  const [qty, setQty] = useState({});
+  // const [products, setProducts] = useState([]);
+  // const [cartItems, setCartItems] = useState(
+  //   JSON.parse(localStorage.getItem("cart"))
+  // );
+  // const [sum, setSum] = useState(0);
+  // const [qty, setQty] = useState({});
 
-  useEffect(() => {
-    Object.keys(cartItems).map(async (key) => {
-      const it = (await axios.get(`http://localhost:8080/products/${key}`))
-        .data;
-      setProducts((prev) => [...prev, { ...it }]);
-      setQty((prev) => ({ ...prev, [key]: cartItems[key] }));
-      if (!localStorage.getItem("price")) {
-        localStorage.setItem("price", JSON.stringify({ [key]: it.price }));
-      } else {
-        const pr = JSON.parse(localStorage.getItem("price"));
-        pr[key] = it.price;
-        localStorage.setItem("price", JSON.stringify(pr));
-      }
-      console.log("key: " + qty[key]);
-      console.log(it.price);
-    });
-  }, []);
-  useEffect(() => {
-    setSum(0);
-    const prices = JSON.parse(localStorage.getItem("price"));
-    Object.keys(qty).map((key) => {
-      setSum((prev) => prev + prices[key] * qty[key]);
-    });
-  }, [qty]);
-  const handle = () => {
-    setSum(0);
-    const prices = JSON.parse(localStorage.getItem("price"));
-    const items = JSON.parse(localStorage.getItem("cart"));
-    Object.keys(prices).map((key) => {
-      setSum((prev) => prev + prices[key] * items[key]);
-    });
-  };
-  const removeItem = (index) => {
-    const temp = cartItems;
-    delete temp[index];
-    setCartItems({ ...temp });
-    console.log(cartItems);
-    console.log(temp);
-    localStorage.setItem("cart", JSON.stringify(temp));
-    location.reload();
-  };
+  // useEffect(() => {
+  //   Object.keys(cartItems).map(async (key) => {
+  //     const it = (await axios.get(`http://localhost:8080/products/${key}`))
+  //       .data;
+  //     setProducts((prev) => [...prev, { ...it }]);
+  //     setQty((prev) => ({ ...prev, [key]: cartItems[key] }));
+  //     if (!localStorage.getItem("price")) {
+  //       localStorage.setItem("price", JSON.stringify({ [key]: it.price }));
+  //     } else {
+  //       const pr = JSON.parse(localStorage.getItem("price"));
+  //       pr[key] = it.price;
+  //       localStorage.setItem("price", JSON.stringify(pr));
+  //     }
+  //     console.log("key: " + qty[key]);
+  //     console.log(it.price);
+  //   });
+  // }, []);
+  // useEffect(() => {
+  //   setSum(0);
+  //   const prices = JSON.parse(localStorage.getItem("price"));
+  //   Object.keys(qty).map((key) => {
+  //     setSum((prev) => prev + prices[key] * qty[key]);
+  //   });
+  // }, [qty]);
+  // const handle = () => {
+  //   setSum(0);
+  //   const prices = JSON.parse(localStorage.getItem("price"));
+  //   const items = JSON.parse(localStorage.getItem("cart"));
+  //   Object.keys(prices).map((key) => {
+  //     setSum((prev) => prev + prices[key] * items[key]);
+  //   });
+  // };
+  // const removeItem = (index) => {
+  //   const temp = cartItems;
+  //   delete temp[index];
+  //   setCartItems({ ...temp });
+  //   console.log(cartItems);
+  //   console.log(temp);
+  //   localStorage.setItem("cart", JSON.stringify(temp));
+  //   location.reload();
+  // };
+ 
+ 
   return (
     <Grid marginTop={6}>
       {products.length > 0 ? (
