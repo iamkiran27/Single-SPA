abf0cae75bd6a76054c88681a81fe2927fd83afd
Sai Kiran <96564277+iamkiran27@users.noreply.github.com>
2022-10-17 15:50:04 +0530

    Revert "setup beffe and add calender crud apis"
    This reverts commit cd3388cd4df49f57c0c7646972144e746b48bc77.

--- beffe/index.ts
+++ beffe/index.ts
@@ -1,8 +1,12 @@
 import { ApolloServer } from 'apollo-server';
+import ProductModule from './product';
+import CartModule from './cart';
 import CalenderModule from './calender';
 
 const server = new ApolloServer({
-  modules: [CalenderModule],
+  modules: [ProductModule, 
+            CartModule,
+            CalenderModule],
   formatError: (err) => {
     return new Error(err.message);
   },
