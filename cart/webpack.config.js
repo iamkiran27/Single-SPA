fc966e3cb0cd36d463271412caa4010374584db5
Sai Kiran <96564277+iamkiran27@users.noreply.github.com>
2022-10-25 11:26:07 +0530

    Ping (#10)
    * Final App
    
    * Final App
    
    * ping1.O
    
    * Login Logout func
    
    * Final
    
    * Cors-rror
    
    Co-authored-by: yashwanthyadav1 <yashwanth.parupudi@zemosolabs.com>

--- cart/webpack.config.js
+++ cart/webpack.config.js
@@ -1,5 +1,6 @@
 const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react");
+const webpack = require("webpack");
 
 module.exports = (webpackConfigEnv, argv) => {
   const defaultConfig = singleSpaDefaults({
@@ -10,6 +11,21 @@ module.exports = (webpackConfigEnv, argv) => {
   });
 
   return merge(defaultConfig, {
+    resolve: {
+      fallback: {
+        //     // assert: require.resolve('assert'),
+        crypto: require.resolve("crypto-browserify"),
+        //     // http: require.resolve('stream-http'),
+        //     // https: require.resolve('https-browserify'),
+        //     // os: require.resolve('os-browserify/browser'),
+        //     // stream: require.resolve('stream-browserify'),
+      },
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        process: "process/browser.js",
+      }),
+    ],
     // modify the webpack config however you'd like to by adding to this object
   });
 };
--- container/src/Container.jsx
+++ container/src/Container.jsx
@@ -1,6 +1,6 @@
 import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Avatar from "@mui/material/Avatar";
 import Badge from "@mui/material/Badge";
 import { pink } from "@mui/material/colors";
@@ -8,9 +8,10 @@ import { Observable } from "windowed-observable";
 import { navigateToUrl } from "single-spa";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
-import { useQuery, gql, useMutation } from "@apollo/client";
-
-
+import { useQuery, gql, useMutation, useLazyQuery } from "@apollo/client";
+import authClient from "./sdk/api";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
 
 const GET_CART_ITEMS = gql`
   query ($userID: String!) {
@@ -28,22 +29,84 @@ const GET_CART_ITEMS = gql`
     }
   }
 `;
+
 const Container = () => {
-  const { loading, error, data } = useQuery(GET_CART_ITEMS, {
-    variables: { userID: "user1" },
-  });
+  const [notification, { loading, error, data }] = useLazyQuery(GET_CART_ITEMS);
 
-  let badge_data = 0;
+  const [notificationData, setnotificationData] = useState();
 
+  // let badge_data = 0;
+  const settings = localStorage.getItem("userId") ? "Log Out" : "Log In";
 
-  if(data)
-  {
-    console.log("data is ", data.CartItems.length);
-    badge_data = data.CartItems.length;
+  // if (data) {
+  //   console.log("data is ", data.CartItems.length);
+  //   badge_data = data.CartItems.length;
 
-  }
+  // }
 
-  console.log("Badge data : ", badge_data);
+  const handleSignIn = () => {
+    // this.clearSession();
+
+    let state = authClient.generateRandomValue();
+    let nonce = authClient.generateRandomValue();
+    // Store state and nonce parameters into the session, so we can retrieve them after
+    // user will be redirected back with access token or code (since react state is cleared in this case)
+    sessionStorage.setItem("state", state);
+    sessionStorage.setItem("nonce", nonce);
+
+    authClient.authorize(state, nonce);
+  };
+  useEffect(() => {
+    console.log("Inside the container");
+    const hashes = authClient.parseHash();
+    
+    console.log("Hashes in containr : ", hashes);
+    if (hashes) {
+      console.log("Inside the hashes, container");
+      authClient
+        .getUserInfo(hashes.access_token)
+        .then((res) => {
+          console.log("Res in calender : ", res);
+          localStorage.setItem("userId", res.preferred_username);
+        })
+        .then((res) => {
+          notification({
+            variables: { userID: localStorage.getItem("userId") },
+          }).then((res) => {
+            setnotificationData(res.data.CartItems.length);
+          });
+        });
+    }
+
+    if (localStorage.getItem("userId")) {
+      notification({
+        variables: { userID: localStorage.getItem("userId") },
+      }).then((res) => {
+        setnotificationData(res.data.CartItems.length);
+      });
+    }
+  }, []);
+
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = (setting) => {
+    setAnchorElUser(null);
+    if (setting == "Log In") {
+      handleSignIn();
+    } else if (setting == "Log Out") {
+      console.log("Log out url");
+      localStorage.removeItem("userId");
+      window.location.replace("/")
+      setnotificationData();
+    }
+  };
 
   // const [notification, setNotification] = useState(
   //   localStorage.getItem("cart") == null
@@ -54,7 +117,6 @@ const Container = () => {
   // observable.subscribe((value) => setNotification(value), { latest: true });
 
   const [location, setLocation] = useState(window.location.pathname);
-  console.log(location);
 
   return (
     <Box
@@ -101,7 +163,7 @@ const Container = () => {
             <CalendarTodayIcon sx={{ color: "white" }} />
           </a>
 
-          <Badge badgeContent={badge_data} color="primary">
+          <Badge badgeContent={notificationData} color="primary">
             <a
               href={`/cart`}
               onClick={navigateToUrl}
@@ -113,7 +175,32 @@ const Container = () => {
               <ShoppingCartIcon sx={{ color: "white" }} />
             </a>
           </Badge>
-          <Avatar sx={{ bgcolor: "#03abab" }}>A</Avatar>
+          <Avatar onClick={handleOpenUserMenu} sx={{ bgcolor: "#03abab" }}>
+            A
+          </Avatar>
+          <Menu
+            sx={{ mt: "45px" }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
+          >
+            <MenuItem
+              key={settings}
+              onClick={() => handleCloseUserMenu(settings)}
+            >
+              <Typography textAlign="center">{settings}</Typography>
+            </MenuItem>
+          </Menu>
         </Box>
       </Box>
     </Box>
